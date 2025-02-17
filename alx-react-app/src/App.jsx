8fcad0464f6a95dc46f4b4198d9bd90e345bec20
Danny2k15 <ds8115925@gmail.com>
2025-02-16 20:33:48 -1200

    all

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,3 @@
-import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -6,8 +5,7 @@ import Footer from './components/Footer';
 
   function App (){
     return (
-      <div>
-        <WelcomeMessage /> 
+      <div> 
         <Header/>
         <MainContent/>
         <Footer/>
