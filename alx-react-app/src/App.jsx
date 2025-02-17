f40839bb00faa5e2c9d6847f123a991871958e35
Danny2k15 <ds8115925@gmail.com>
2025-02-16 19:58:05 -1200

    all

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,13 +2,12 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-import './App.css'
 
 
-  const App = () => {
+  function App (){
     return (
       <div>
-        <WelcomeMessage />  {/* Include the WelcomeMessage component here */}
+        <WelcomeMessage /> 
         <Header/>
         <MainContent/>
         <Footer/>
