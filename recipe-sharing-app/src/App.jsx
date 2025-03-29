f2378512acb158ce9e307e636958a0b98214eb88
Danny2k15 <ds8115925@gmail.com>
2025-03-29 02:14:19 -1200

    all

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,11 @@
-import "./App.css";
-import "./Components/AddRecipeForm";
 import AddRecipeForm from "./Components/AddRecipeForm";
-import "./Components/RecipeList";
 import RecipeList from "./Components/RecipeList";
 
 function App() {
   return (
     <div>
-      <AddRecipeForm />
       <RecipeList />
+      <AddRecipeForm />
     </div>
   );
 }
