a0f71a22f5e06e679ffee323fc43b949baa07a53
Danny2k15 <ds8115925@gmail.com>
2025-03-29 02:09:52 -1200

    all

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import "./Components/AddRecipeForm";
+import AddRecipeForm from "./Components/AddRecipeForm";
+import "./Components/RecipeList";
+import RecipeList from "./Components/RecipeList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div></div>;
+  return (
+    <div>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
 }
 
 export default App;
