75c14725f6b6732ac490462228b60048ef24bbb5
Danny2k15 <ds8115925@gmail.com>
2025-03-29 02:19:45 -1200

    all

--- recipe-sharing-app/src/Components/RecipeList.jsx
+++ recipe-sharing-app/src/Components/RecipeList.jsx
@@ -1,5 +1,4 @@
-// RecipeList component
-import { useRecipeStore } from "./recipeStore";
+import useRecipeStore from "./recipeStore";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
