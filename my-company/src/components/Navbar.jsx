1ab181172bd9db5c0756fa88aecfb2a360567077
Danny2k15 <ds8115925@gmail.com>
2025-03-29 01:24:42 -1200

    All

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,14 +1,32 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-      <nav style={{ padding: "10px", background: "#333", color: "white" }}>
-        <Link to="/" style={{ margin: "10px", color: "white" }}>Home</Link>
-        <Link to="/about" style={{ margin: "10px", color: "white" }}>About</Link>
-        <Link to="/services" style={{ margin: "10px", color: "white" }}>Services</Link>
-        <Link to="/contact" style={{ margin: "10px", color: "white" }}>Contact</Link>
-      </nav>
-    );
-  }
+  return (
+    <nav
+      style={{
+        padding: "10px",
+        background: "#333",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "20px",
+      }}
+    >
+      <Link to="/" style={{ margin: "10px", color: "white" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ margin: "10px", color: "white" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ margin: "10px", color: "white" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ margin: "10px", color: "white" }}>
+        Contact
+      </Link>
+    </nav>
+  );
+}
 
-  export default Navbar;
+export default Navbar;
