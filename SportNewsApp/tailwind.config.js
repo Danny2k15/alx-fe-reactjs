a21b96d6fe5eb2ca5aadf72c6e1888a8d2ea36a5
Danny2k15 <ds8115925@gmail.com>
2025-04-06 15:03:57 +0000

    All

---
+++ SportNewsApp/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure this includes all React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
