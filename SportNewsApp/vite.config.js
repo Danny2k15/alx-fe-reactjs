c550883b6ffdcc0243430a9a1c6fde7548b1d679
Danny2k15 <ds8115925@gmail.com>
2025-04-10 19:20:29 +0000

    all

--- SportNewsApp/vite.config.js
+++ SportNewsApp/vite.config.js
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react-swc";
 // Export config
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy", 
   optimizeDeps: {
-    exclude: ["react-router-dom", "axios"], // Exclude dependencies that are causing issues
+    exclude: ["react-router-dom", "axios"],
   },
 });
