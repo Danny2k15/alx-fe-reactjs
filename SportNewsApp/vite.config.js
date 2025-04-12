344554d63ae1df00b366bb9b66b2214017750f15
dwaynissh <dwaynissh@gmail.com>
2025-04-12 12:31:46 +0100

    All

--- SportNewsApp/vite.config.js
+++ SportNewsApp/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +8,8 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: "/index.html",
+    },
   },
 });
